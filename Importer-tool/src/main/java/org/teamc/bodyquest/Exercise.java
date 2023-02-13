14607cc5a0428428e3aed911b6cf8dc8d3be5b1a
Davit Voskerchyan <davitvoskerchyan@outlook.com>
2023-02-13 12:17:32 -0500

    Add Basic project

---
+++ Importer-tool/src/main/java/org/teamc/bodyquest/Exercise.java
@@ -0,0 +1,9 @@
+package org.teamc.bodyquest;
+
+import org.bson.codecs.pojo.annotations.BsonProperty;
+import org.bson.*;
+import org.bson.types.*;
+import java.util.*;
+
+public class Exercise {
+}
---
+++ Importer-tool/src/main/java/org/teamc/bodyquest/Main.java
@@ -0,0 +1,32 @@
+package org.teamc.bodyquest;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.List;
+
+public class Main {
+    static String URL = "mongodb+srv://Davit:davit@cluster0.ktirqdm.mongodb.net/?retryWrites=true&w=majority";
+    public static void main(String[] args) {
+        ConnectionString connectionString = new ConnectionString(URL);
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .serverApi(ServerApi.builder()
+                        .version(ServerApiVersion.V1)
+                        .build())
+                .build();
+        MongoClient mongoClient = MongoClients.create(settings);
+        MongoDatabase database = mongoClient.getDatabase("body-quest");
+
+        MongoCollection<Document> colExercises = database.getCollection("exercises");
+        colExercises.drop();
+        database.createCollection("exercises");
+    }
+}
