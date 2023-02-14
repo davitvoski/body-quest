fe9f84f2c0d7a2b7346317f20bbdb08e51761b79
Davit Voskerchyan <davitvoskerchyan@outlook.com>
2023-02-13 20:32:37 -0500

    Added Tests for ImportingFile

--- Importer-tool/src/main/java/org/teamc/bodyquest/io/ImporterCSV.java
+++ Importer-tool/src/main/java/org/teamc/bodyquest/io/ImporterCSV.java
@@ -1,16 +1,8 @@
 package org.teamc.bodyquest.io;
 
-import com.mongodb.lang.NonNull;
-import com.mongodb.lang.Nullable;
-import org.bson.types.ObjectId;
 import org.teamc.bodyquest.Exercise;
-
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URISyntaxException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -22,7 +14,7 @@ import java.util.List;
  * This class imports the dataset from a CSV file into an ArrayList of Exercise objects.
  */
 public class ImporterCSV {
-    private String fileName = "fitness-exercises.csv";
+    private String fileName;
 
     /**
      * Constructor for the ImporterCSV class with a file name
@@ -42,6 +34,7 @@ public class ImporterCSV {
         List<String> lines = readCSV();
         ArrayList<Exercise> exercises = new ArrayList<>();
         for (String line: lines) {
+            if(line.equals(lines.get(0))) continue; // Skip first line (header
             String[] splitLine = line.split(",");
             Exercise exercise = new Exercise().setBodyPart(splitLine[0])
                     .setEquipment(splitLine[1])
@@ -60,7 +53,7 @@ public class ImporterCSV {
      * @throws URISyntaxException if the file is not found
      * @throws IOException if the file could not be read
      */
-    private List<String> readCSV() throws URISyntaxException, IOException {
+    public List<String> readCSV() throws URISyntaxException, IOException {
         var url = getClass().getClassLoader().getResource(this.fileName);
         assert url != null;
         Path path = Paths.get(url.toURI());
---
+++ Importer-tool/src/test/java/org/teamc/bodyquest/io/ImporterCSVTest.java
@@ -0,0 +1,118 @@
+package org.teamc.bodyquest.io;
+
+import org.junit.jupiter.api.Test;
+import org.teamc.bodyquest.Exercise;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImporterCSVTest {
+
+    @Test
+    void loadCSV() {
+        ImporterCSV importer = new ImporterCSV("fitness_exercises.csv");
+        try {
+            var data = importer.loadCSV();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Error while loading CSV file");
+        }
+    }
+
+    @Test
+    void isSecondExerciseCorrectlyLoaded() {
+        ImporterCSV importer = new ImporterCSV("fitness_exercises.csv");
+        try {
+            var data = importer.loadCSV();
+            var exercise = data.get(1);
+            assertEquals("waist", exercise.getBodyPart());
+            assertEquals("body weight", exercise.getEquipment());
+            assertEquals("http://d205bpvrqc9yn1.cloudfront.net/0002.gif", exercise.getGifUrl());
+            assertEquals("45° side bend", exercise.getName());
+            assertEquals("abs", exercise.getTarget());
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Error while loading CSV file");
+        }
+    }
+    @Test
+    void isBeforeLastExerciseCorrectlyLoaded() {
+        ImporterCSV importer = new ImporterCSV("fitness_exercises.csv");
+        try {
+            var data = importer.loadCSV();
+            var exercise = data.get(data.size()-2);
+            assertEquals("lower arms", exercise.getBodyPart());
+            assertEquals("body weight", exercise.getEquipment());
+            assertEquals("http://d205bpvrqc9yn1.cloudfront.net/1428.gif", exercise.getGifUrl());
+            assertEquals("wrist circles", exercise.getName());
+            assertEquals("forearms", exercise.getTarget());
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Error while loading CSV file");
+        }
+    }
+
+    @Test
+    void readCSV() {
+        ImporterCSV importer = new ImporterCSV("fitness_exercises.csv");
+        try {
+            importer.readCSV();
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Error while reading CSV file");
+        }
+    }
+
+    @Test
+    void isFirstLineOfDataSetCorrect() {
+        ImporterCSV importer = new ImporterCSV("fitness_exercises.csv");
+        try {
+            var data = importer.readCSV();
+            var firstLine = data.get(1).split(",");
+            assertEquals("waist", firstLine[0]);
+            assertEquals("body weight", firstLine[1]);
+            assertEquals("http://d205bpvrqc9yn1.cloudfront.net/0001.gif", firstLine[2]);
+            assertEquals("0001", firstLine[3]);
+            assertEquals("3/4 sit-up", firstLine[4]);
+            assertEquals("abs", firstLine[5]);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Error while reading CSV file");
+        }
+    }
+
+    @Test
+    void isLastLineOfDataSetCorrect() {
+        ImporterCSV importer = new ImporterCSV("fitness_exercises.csv");
+        try {
+            var data = importer.readCSV();
+            var lastLine = data.get(data.size() - 1).split(",");
+            assertEquals("lower arms", lastLine[0]);
+            assertEquals("weighted", lastLine[1]);
+            assertEquals("http://d205bpvrqc9yn1.cloudfront.net/0859.gif", lastLine[2]);
+            assertEquals("0859", lastLine[3]);
+            assertEquals("wrist rollerer", lastLine[4]);
+            assertEquals("forearms", lastLine[5]);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Error while reading CSV file");
+        }
+    }
+
+    @Test
+    void isHeadersLineOfDataSetCorrect() {
+        ImporterCSV importer = new ImporterCSV("fitness_exercises.csv");
+        try {
+            var data = importer.readCSV();
+            var lastLine = data.get(0).split(",");
+            assertEquals("bodyPart", lastLine[0]);
+            assertEquals("equipment", lastLine[1]);
+            assertEquals("gifUrl", lastLine[2]);
+            assertEquals("id", lastLine[3]);
+            assertEquals("name", lastLine[4]);
+            assertEquals("target", lastLine[5]);
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail("Error while reading CSV file");
+        }
+    }
+}
