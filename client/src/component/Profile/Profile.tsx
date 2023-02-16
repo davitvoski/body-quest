5c243e73a5058ad9dd1bf409cf62a3c12d4a94bb
Sophia M <75237236+themarshgirl@users.noreply.github.com>
2023-02-16 13:41:33 -0500

    Split Profile into Profile and ProfileView

--- client/src/component/Profile/Profile.tsx
+++ client/src/component/Profile/Profile.tsx
@@ -1,5 +1,6 @@
 import { Avatar, Grid, Paper, styled } from "@mui/material";
 import { JSXElementConstructor, ReactElement, ReactFragment, ReactPortal, useState } from "react";
+import { ProfileView } from "./ProfileView";
 
 const Item = styled(Paper)(({ theme }) => ({
     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -9,40 +10,14 @@ const Item = styled(Paper)(({ theme }) => ({
     color: theme.palette.text.secondary,
   }));
 
-export const Profile = (props: { username: string; email: string; }) => {
-    const [username, setUsername] = useState()
-    const [email, setEmail] = useState()
+export const Profile = () => {
+    const [username, setUsername] = useState("")
+    const [email, setEmail] = useState("")
     const [level, setLevel] = useState()
 
     return(
         <div className="profile">
-            <Grid container spacing={4} sx={{ padding: "5%" }}>
-                <Grid item xs={4}>
-                    <Item sx={{ width: "auto", height: "100%" }}>
-                        <Avatar 
-                            alt={ props.username } 
-                            src="" 
-                            variant="rounded"
-                            sx={{ width: "auto", height: "100%", margin: "auto" }}/>
-                    </Item>
-                </Grid>
-                <Grid item xs>
-                    <Grid container spacing={4}>
-                        <Grid item xs={12}>
-                            <Item>{ props.username }</Item>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Item>{ props.email }</Item>
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Item>bio here</Item>
-                        </Grid>
-                    </Grid>
-                </Grid>
-                <Grid item xs={12}>
-                    <Item>this will be the xp bar</Item>
-                </Grid>
-            </Grid>
+            <ProfileView username={username} email={email}></ProfileView>
         </div>
     )
 }
---
+++ client/src/component/Profile/ProfileView.tsx
@@ -0,0 +1,42 @@
+import { Avatar, Grid, Paper, styled } from "@mui/material";
+import { JSXElementConstructor, ReactElement, ReactFragment, ReactPortal, useState } from "react";
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body1,
+    padding: theme.spacing(1),
+    textAlign: 'left',
+    color: theme.palette.text.secondary,
+  }));
+
+export const ProfileView = (props: { username: string; email: string; }) => {
+    return(
+        <Grid container spacing={4} sx={{ padding: "5%" }}>
+            <Grid item xs={4}>
+                <Item sx={{ width: "auto", height: "100%" }}>
+                    <Avatar 
+                        alt={ props.username } 
+                        src="" 
+                        variant="rounded"
+                        sx={{ width: "auto", height: "100%", margin: "auto" }}/>
+                </Item>
+            </Grid>
+            <Grid item xs>
+                <Grid container spacing={4}>
+                    <Grid item xs={12}>
+                        <Item>{ props.username }</Item>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Item>{ props.email }</Item>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Item>bio here</Item>
+                    </Grid>
+                </Grid>
+            </Grid>
+            <Grid item xs={12}>
+                <Item>this will be the xp bar</Item>
+            </Grid>
+        </Grid>
+    )
+}
