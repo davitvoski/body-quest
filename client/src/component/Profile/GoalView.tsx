875301e7dbf12a520df189448e80bcc2c0d52633
Sophia Marshment <themarshments@gmail.com>
2023-02-23 12:57:11 -0500

    List goals and sort by checkboxes

--- client/src/component/Profile/GoalView.tsx
+++ client/src/component/Profile/GoalView.tsx
@@ -1,33 +1,57 @@
+import { Checkbox } from "@mui/material";
 import { Item } from "../modules/Item";
 
 const tempGoals = [
     {
-        "username": "john",
-        "goal": "ankle circles",
+        "name": "ankle circles",
         "reps": 20,
         "completed": false
     }, 
     {
-        "username": "jane",
-        "goal": "band bench press",
+        "name": "band bench press",
+        "reps": 30,
+        "completed": true
+    }, 
+    {
+        "name": "dumbbell deadlift",
+        "reps": 40,
+        "completed": false
+    },
+    {
+        "name": "ankle circles",
+        "reps": 20,
+        "completed": true
+    }, 
+    {
+        "name": "band bench press",
         "reps": 30,
         "completed": false
     }, 
     {
-        "username": "doe",
-        "goal": "dumbbell deadlift",
+        "name": "dumbbell deadlift",
         "reps": 40,
-        "completed": false
+        "completed": true
     }
 ]
 
 export const GoalView = () => {
+    const isCompleted = (goal: any) => goal.completed;
+    const isIncomplete = (goal: any) => !goal.completed;
+
     return(
         <div>
-            { tempGoals.map(goals => 
-            <Item sx={{ m: "1% 0 1% 0"}}>
-
-            </Item>) }
+            { tempGoals.filter(isIncomplete).map(goal => 
+                <Item sx={{ m: "1% 0 1% 0", p:2}}>
+                    <Checkbox sx={{ color: "white"}} />
+                    { goal.reps } reps: { goal.name }
+                </Item>
+            )}
+            { tempGoals.filter(isCompleted).map(goal => 
+                <Item sx={{ m: "1% 0 1% 0", p:2, color: "text.disabled"}}>
+                    <Checkbox sx={{ color: "white"}} disabled checked />
+                    { goal.reps } reps: { goal.name }
+                </Item>
+            )}
         </div>
     )
 }
