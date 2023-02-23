19597c858d86771e7c2846686806468719351ec2
wanting huang <huangwanting068@gmail.com>
2023-02-23 18:02:36 -0500

    change font size to uppercase for exercise

--- client/src/component/Exercise/Exercise.tsx
+++ client/src/component/Exercise/Exercise.tsx
@@ -8,8 +8,8 @@ export const Exercise=(props:ExerciseProps)=>{
     return (
         <div className="exerciseArea">
             <Card className="cardExecise" variant="outlined">
-                <CardContent>
-                    <Typography>{props.exercise.name}</Typography>
+                <CardContent className="exerciseContent">
+                    <Typography className="exerciseNames">{props.exercise.name}</Typography>
                     <Typography>{props.exercise.body_part}</Typography>
                 </CardContent>
             </Card>
