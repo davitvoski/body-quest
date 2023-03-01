7b5f9ce7b223699dde41ac43a9c5f05b85f7b739
Davit_V <davit.voskerchyan@dawsoncollege.qc.ca>
2023-03-01 12:24:58 -0500

    Add goal Type

--- shared/index.d.ts
+++ shared/index.d.ts
@@ -1,8 +1,27 @@
-export interface IExercise{
+export interface IExercise {
     _id: string,
     name: string,
     target: string,
     gifUrl: string,
     equipment: string,
     body_part: string,
+}
+
+export interface IUser {
+    username: string,
+    email: string,
+    avatar: string,
+    goals: IGoal[],
+}
+
+//TODO: Talk to @santigao and @sophia about this
+export interface IGoal {
+    title: string,
+    // TODO: Think if a string would be more appropriate
+    exercise: IExercise,
+    reps: number,
+    sets?: number,
+    // In KG
+    weight?: number,
+    completed: false
 }
