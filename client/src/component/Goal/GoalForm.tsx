0b5a089e47d1f1bdae59f7f88038695c13fba9ea
santi <santiluna03@outlook.com>
2023-03-06 23:18:52 -0500

    adding form validation

--- client/src/component/Goal/GoalForm.tsx
+++ client/src/component/Goal/GoalForm.tsx
@@ -1,8 +1,11 @@
 import {
+  Button,
   FormControl,
   Grid,
+  InputAdornment,
   InputLabel,
   MenuItem,
+  Paper,
   Select,
   SelectChangeEvent,
   Stack,
@@ -17,65 +20,148 @@ import "../../styles/GoalForm.css";
 export const GoalForm = (props: any) => {
   let { state } = useLocation();
   const [goalType, setGoalType] = React.useState("");
-  const handleChange = (event: SelectChangeEvent) => {
-    setGoalType(event.target.value as string);
+  const [goalValue, setGoalValue] = React.useState("");
+  const [dateValue, setDateValue] = React.useState("");
+  const [isValid, setIsValid] = React.useState(false);
+  const [errors, setErrors] = React.useState({ goalValue: "", endDate: "" });
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let name = event.target.name;
+    console.log(name);
+
+    switch (name) {
+      case "type":
+        setGoalType(event.target.value as string);
+        break;
+      case "amount":
+        if (event.target.value.length > 0) {
+          setGoalValue(event.target.value as string);
+        } else {
+        }
+        break;
+      case "date":
+        setDateValue(event.target.value as string);
+        break;
+    }
+  };
+
+  const checkFormValid = () => {
+    if (goalValue !== "") {
+      setIsValid(true);
+    }
+  };
+
+  function handleDateChange(newValue: any) {
+    console.log(newValue);
+  }
+
+  const handleGoalValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setGoalValue(event.target.value as string);
   };
 
   return (
     <div className="form-container">
-      <Typography variant="h2" component="h2">
-        CREATE GOAL FOR {state.exerciseName}
-      </Typography>
-      <form className="goal-form">
-        <Stack justifyContent="center" alignItems="center" spacing={5}>
-          <Stack
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-            spacing={8}
-          >
-            <FormControl sx={{ m: 1, minWidth: 80 }}>
-              <InputLabel htmlFor="goal-type-label">Type</InputLabel>
-              <Select
-                autoWidth
-                labelId="goal-type-label"
-                id="goal-type-select"
-                value={goalType}
-                label="Goal"
-                onChange={handleChange}
-                sx={{ backgroundColor: "white" }}
-              >
-                <MenuItem value={"reps"}>Reps</MenuItem>
-                <MenuItem value={"weight"}>Weight</MenuItem>
-                <MenuItem value={"time"}>Time</MenuItem>
-              </Select>
-            </FormControl>
-            <FormControl>
-              <TextField
-                label="Amount"
-                id="goal-amount-input"
-                variant="outlined"
-                type="number"
-                inputProps={{ min: 0 }}
-              />
-            </FormControl>
-          </Stack>
-          <Stack
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-            spacing={5}
-          >
-            <FormControl>
-              <ResponsiveDataPicker label={"Start Date"} />
-            </FormControl>
+      <Paper elevation={3} sx={{ maxWidth: "50%" }}>
+        <div className="header">
+          <Typography variant="h4" component="h4">
+            Goal Creation: {state.exerciseName}
+          </Typography>
+        </div>
+        <form className="goal-form">
+          <Stack justifyContent="center" alignItems="center" spacing={5}>
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              spacing={8}
+            >
+              <FormControl sx={{ m: 1, minWidth: 80 }}>
+                <InputLabel htmlFor="goal-type-label">Type</InputLabel>
+                <Select
+                  autoWidth
+                  labelId="goal-type-label"
+                  id="goal-type-select"
+                  value={goalType}
+                  label="Goal"
+                  onChange={(event: SelectChangeEvent) => {
+                    setGoalType(event.target.value as string);
+                  }}
+                >
+                  <MenuItem value={"reps"}>Reps</MenuItem>
+                  <MenuItem value={"weight"}>Weight</MenuItem>
+                  <MenuItem value={"time"}>Time</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl>
+                {
+                  {
+                    reps: (
+                      <TextField
+                        required
+                        name="amount"
+                        label="Amount"
+                        id="goal-amount-input"
+                        variant="filled"
+                        type="number"
+                        inputProps={{ min: 0 }}
+                        sx={{ color: "white" }}
+                        onChange={handleChange}
+                      />
+                    ),
+                    weight: (
+                      <TextField
+                        name="amount"
+                        label="Kg"
+                        id="goal-amount-input"
+                        variant="filled"
+                        type="number"
+                        inputProps={{ min: 0 }}
+                        onChange={handleChange}
+                      />
+                    ),
+                    time: (
+                      <TextField
+                        label="Sec"
+                        name="amount"
+                        id="goal-amount-input"
+                        variant="filled"
+                        type="number"
+                        inputProps={{
+                          min: 0,
+                        }}
+                        onChange={handleChange}
+                      />
+                    ),
+                  }[goalType]
+                }
+              </FormControl>
+            </Stack>
+            <Stack
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+              spacing={5}
+            >
+              <FormControl>
+                <ResponsiveDataPicker label={"Start Date"} isToday={true} />
+              </FormControl>
 
-            <FormControl>
-              <ResponsiveDataPicker label={"End Date"} />
-            </FormControl>
+              <FormControl>
+                <ResponsiveDataPicker
+                  label={"End Date"}
+                  isToday={false}
+                  onChange={handleDateChange}
+                />
+              </FormControl>
+            </Stack>
           </Stack>
-        </Stack>
-      </form>
+          {isValid ? (
+            <Button variant="contained">Create</Button>
+          ) : (
+            <Button disabled>Create</Button>
+          )}
+        </form>
+      </Paper>
     </div>
   );
 };
--- client/src/component/ResponsiveDataPicker.tsx
+++ client/src/component/ResponsiveDataPicker.tsx
@@ -1,25 +1,28 @@
-import * as React from 'react';
-import dayjs, { Dayjs } from 'dayjs';
-import TextField from '@mui/material/TextField';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import * as React from "react";
+import dayjs, { Dayjs } from "dayjs";
+import TextField from "@mui/material/TextField";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
 export default function ResponsiveDatePicker(props: any) {
-  const [value, setValue] = React.useState<Dayjs | null>(dayjs());
+  const [value, setValue] = React.useState<Dayjs | null>(
+    props.isToday ? dayjs() : null
+  );
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DatePicker
-          label={props.label}
-          openTo="day"
-          views={['year', 'month', 'day']}
-          value={value}
-          onChange={(newValue) => {
-            setValue(newValue);
-          }}
-          renderInput={(params) => <TextField {...params} />}
-        />
+      <DatePicker
+        label={props.label}
+        openTo="day"
+        views={["year", "month", "day"]}
+        value={value}
+        onChange={(newValue) => {
+          props.onChange(newValue);
+          setValue(newValue);
+        }}
+        renderInput={(params) => <TextField {...params} />}
+      />
     </LocalizationProvider>
   );
 }
