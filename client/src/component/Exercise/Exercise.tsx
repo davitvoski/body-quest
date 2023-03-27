2ac34557ed86cfbdd07c0319b06f7cde63a4c96f
Sophia Marshment <themarshments@gmail.com>
2023-03-26 22:16:34 -0400

    Clean up code for card icons

--- client/src/component/Exercise/Exercise.tsx
+++ client/src/component/Exercise/Exercise.tsx
@@ -22,6 +22,25 @@ export const Exercise = (props: ExerciseProps) => {
     setIsOpen(!isOpen);
   };
 
+  const renderImage = (mode: string, bodyPart: string) => {
+    let src = "/body-parts/"+mode+"-mode/"
+
+    switch(bodyPart) {
+      case "waist" : src += "waist.png"; break;
+      case "upper legs" : src += "upper-leg.png"; break;
+      case "back" : src += "back.png"; break;
+      case "lower legs" : src += "lower-leg.png"; break;
+      case "chest" : src += "chest.png"; break;
+      case "upper arms" : src += "upper-arm.png"; break;
+      case "cardio" : src += "cardio.png"; break;
+      case "shoulders" : src += "shoulder.png"; break;
+      case "lower arm" : src += "lower-arm.png"; break;
+      case "neck" : src += "neck.png"; break;
+    }
+    
+    return ( <img width={90} loading="lazy" src={ src } /> )
+  }
+
   return (
     <>
       <div className="exerciseArea" onClick={handlePopup}>
@@ -29,75 +48,7 @@ export const Exercise = (props: ExerciseProps) => {
           <CardContent className="exerciseContent">
             <Typography className="exerciseNames">{props.exercise.name}</Typography>
             <div className="img-container">
-              {props.exercise.body_part === "waist" &&
-                (theme.palette.mode === "dark" ? (
-                  <img width={90} loading="lazy" src="/body-parts/light-mode/waist-white.png" />
-                ) : (
-                  <img width={90} loading="lazy" src="/body-parts/dark-mode/waist.png" />
-                ))}
-
-              {props.exercise.body_part === "upper legs" &&
-                (theme.palette.mode === "dark" ? (
-                  <img width={90} loading="lazy" src="/body-parts/light-mode/upper-leg-white.png" />
-                ) : (
-                  <img width={90} loading="lazy" src="/body-parts/dark-mode/upper-leg.png" />
-                ))}
-
-              {props.exercise.body_part === "back" &&
-                (theme.palette.mode === "dark" ? (
-                  <img width={120} loading="lazy" src="/body-parts/light-mode/back-white.png" />
-                ) : (
-                  <img width={120} loading="lazy" src="/body-parts/dark-mode/back.png" />
-                ))}
-
-              {props.exercise.body_part === "lower legs" &&
-                (theme.palette.mode === "dark" ? (
-                  <img width={90} loading="lazy" src="/body-parts/light-mode/lower-leg-white.png" />
-                ) : (
-                  <img width={90} loading="lazy" src="/body-parts/dark-mode/lower-leg.png" />
-                ))}
-
-              {props.exercise.body_part === "chest" &&
-                (theme.palette.mode === "dark" ? (
-                  <img width={110} loading="lazy" src="/body-parts/light-mode/chest-white.png" />
-                ) : (
-                  <img width={110} loading="lazy" src="/body-parts/dark-mode/chest.png" />
-                ))}
-
-              {props.exercise.body_part === "upper arms" &&
-                (theme.palette.mode === "dark" ? (
-                  <img width={90} loading="lazy" src="/body-parts/light-mode/upper-arm-white.png" />
-                ) : (
-                  <img width={90} loading="lazy" src="/body-parts/dark-mode/upper-arm.png" />
-                ))}
-
-              {props.exercise.body_part === "cardio" &&
-                (theme.palette.mode === "dark" ? (
-                  <img width={90} loading="lazy" src="/body-parts/light-mode/cardio-white.png" />
-                ) : (
-                  <img width={90} loading="lazy" src="/body-parts/dark-mode/cardio.png" />
-                ))}
-
-              {props.exercise.body_part === "shoulders" &&
-                (theme.palette.mode === "dark" ? (
-                  <img width={90} loading="lazy" src="/body-parts/light-mode/shoulder-white.png" />
-                ) : (
-                  <img width={90} loading="lazy" src="/body-parts/dark-mode/shoulder.png" />
-                ))}
-
-              {props.exercise.body_part === "lower arms" &&
-                (theme.palette.mode === "dark" ? (
-                  <img width={90} loading="lazy" src="/body-parts/light-mode/lower-arm-white.png" />
-                ) : (
-                  <img width={90} loading="lazy" src="/body-parts/dark-mode/lower-arm.png" />
-                ))}
-
-              {props.exercise.body_part === "neck" &&
-                (theme.palette.mode === "dark" ? (
-                  <img width={90} loading="lazy" src="/body-parts/light-mode/neck-white.png" />
-                ) : (
-                  <img width={90} loading="lazy" src="/body-parts/dark-mode/neck.png" />
-                ))}
+              { renderImage(theme.palette.mode, props.exercise.body_part) }
             </div>
           </CardContent>
         </Card>
