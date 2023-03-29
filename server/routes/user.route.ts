22761d00859ebd94188f2a4fb480ff1151a40c03
Davit_V <davit.voskerchyan@dawsoncollege.qc.ca>
2023-03-29 10:29:35 -0400

    add logic to add image to db + update user info

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,9 +1,20 @@
+import { BlobServiceClient } from "@azure/storage-blob";
 import express, { Request, Response } from "express";
 import { isAuthenticated } from "../controllers/auth.controller";
+import Database from "../database/db";
 
 const userRouter = express.Router()
 
-userRouter.patch("/", isAuthenticated, (req: Request, res: Response) => {
+const containerName = "bodyquestprofilepictures";
+const sasToken = process.env.AZURE_STORAGE_SAS_TOKEN;
+const storageAccountName = process.env.AZURE_STORAGE_RESOURCE_NAME;
+const uploadUrl = `https://${storageAccountName}.blob.core.windows.net/?${sasToken}`;
+const blobService = new BlobServiceClient(uploadUrl);
+const containerClient = blobService.getContainerClient(containerName);
+
+
+
+userRouter.patch("/", isAuthenticated, async (req: Request, res: Response) => {
     console.log("body patch /users", req.body)
     console.log("session user", req.session.user)
     console.log(req.body.username === req.session.user?.username)
@@ -11,19 +22,45 @@ userRouter.patch("/", isAuthenticated, (req: Request, res: Response) => {
 
     console.log(req.body.avatar === req.session.user?.picture)
 
+    const newUserImage = req.body.avatar
+    const newUsername = req.body.username
     // Check for changes
-    if (req.body.username === req.session.user?.username &&
-        req.body.avatar === req.session.user?.picture) {
-
+    if (newUsername === req.session.user?.username &&
+        newUserImage === req.session.user?.picture) {
         return res.status(204).send("No changes.")
     }
 
     // Add image to blob storage
-
+    const azureImageUrl = await addProfilePictureToAzure(newUserImage, newUsername)
+    const user = new Database().updateUserInformation(newUsername, azureImageUrl, req.session.user?.email)
     // Change user in database.
     res.sendStatus(404)
 
 
 })
+
+/**
+/**
+ * This function saves the image to the azure blob storage using the caption and current time,
+ * this is done so that no other file will have the same name
+ * @param file base64 string
+ * @param caption string
+ */
+async function addProfilePictureToAzure(file: string, username: string) {
+    const secondhalf = file.split(":")[1];
+    const mimetype = secondhalf.split(";")[0];
+    var today = Date.now();
+
+    const fileName = `${username}profile${today}.png`
+    const blobClient = containerClient.getBlockBlobClient(fileName.toLocaleLowerCase());
+    const options = { blobHTTPHeaders: { blobContentType: mimetype } };
+
+    const base64Image = file.split(';base64,').pop() as string;
+    var buf = Buffer.from(base64Image, 'base64');
+
+    await blobClient.uploadData(buf, options);
+    let imageUrl = `https://${storageAccountName}.blob.core.windows.net/${containerName}/${fileName.toLocaleLowerCase()}}`
+    return imageUrl;
+}
 
 export default userRouter
