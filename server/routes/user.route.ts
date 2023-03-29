df92a954e4d9daabced57eda9821e65ca55fc095
Davit_V <davit.voskerchyan@dawsoncollege.qc.ca>
2023-03-28 20:08:57 -0400

    Check functions

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -3,9 +3,26 @@ import { isAuthenticated } from "../controllers/auth.controller";
 
 const userRouter = express.Router()
 
-userRouter.put("/", isAuthenticated, (req: Request, res: Response) => {
-    if (req.body.email != req.session.user?.email) throw new Error("Wrong User")
-    if (req.body.username === req.session.user?.username && req.body.image === req.session.user?.picture) return res.send("No changes.")
+userRouter.patch("/", isAuthenticated, (req: Request, res: Response) => {
+    console.log("body patch /users", req.body)
+    console.log("session user", req.session.user)
+    console.log(req.body.username === req.session.user?.username)
+    console.log(req.session.user?.picture.localeCompare(req.body.avatar))
+
+    console.log(req.body.avatar === req.session.user?.picture)
+
+    // Check for changes
+    if (req.body.username === req.session.user?.username &&
+        req.body.avatar === req.session.user?.picture) {
+
+        return res.status(204).send("No changes.")
+    }
+
+    // Add image to blob storage
+
+    // Change user in database.
+    res.sendStatus(404)
+
 
 })
 
